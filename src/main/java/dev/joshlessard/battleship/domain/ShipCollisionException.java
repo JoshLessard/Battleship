e6f73456bb1de8dd45efa8a0419ab5e2e223dba4
Josh Lessard <jlessard@axonify.com>
2021-06-27 21:58:00 -0400

    TODO

--- src/main/java/dev/joshlessard/battleship/domain/ShipCollisionException.java
+++ src/main/java/dev/joshlessard/battleship/domain/ShipCollisionException.java
@@ -1,4 +1,6 @@
 package dev.joshlessard.battleship.domain;
 
+// todo: Trace all paths that can throw exceptions and make sure they're handled properly in UI
+//  May need to add more information to the exception (e.g., the two colliding ships here)
 class ShipCollisionException extends RuntimeException {
 }
